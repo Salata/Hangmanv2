84950f4d40537bb7887273ceb74335c468452876
Salata <snoopylettuce25@gmail.com>
2018-01-14 22:48:27 +0100

    clean start

--- Controller.java
+++ Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
 public class Controller {
+    public void asd() {
+        String asd = "asd";
+        String bds = "asd";
+        if (asd.equals(bds)) {
+            System.out.println("sad");
+        }
+    }
 }
